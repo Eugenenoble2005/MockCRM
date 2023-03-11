f602c062fa0f74537e94afb0d27d8ebb7d9f382b
Eugene Noble <71329328+Eugenenoble2005@users.noreply.github.com>
2023-03-11 19:30:01 +0100

    new commit

--- ClientApp/src/app/services/api.service.ts
+++ ClientApp/src/app/services/api.service.ts
@@ -8,7 +8,7 @@ export class ApiService {
   public base_url: string;
   auth_headers: { auth_token: string; };
   constructor(public _cookie: CookieService) {
-    this.base_url = "https://localhost:7278/api"
+    this.base_url = "http://139.59.154.185/server/api"
 
     let token = this._cookie.get("token");
     this.auth_headers = { "auth_token": `${token}` };
